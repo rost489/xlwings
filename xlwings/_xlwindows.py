f2bfcde7d19347814f98406d3c004011d6b4f37b
Felix Zumstein <fzumstein@gmail.com>
2020-04-19 13:26:06 +0200

    wip

--- xlwings/_xlwindows.py
+++ xlwings/_xlwindows.py
@@ -1237,7 +1237,7 @@ class ListObject:
 
     @property
     def data_body_range(self):
-        return Range(xl=self.xl.DataBodyRange)
+        return Range(xl=self.xl.DataBodyRange) if self.xl.DataBodyRange else None
 
     @property
     def display_name(self):
@@ -1246,6 +1246,10 @@ class ListObject:
     @property
     def header_row_range(self):
         return Range(xl=self.xl.HeaderRowRange)
+
+    @property
+    def insert_row_range(self):
+        return Range(xl=self.xl.InsertRowRange)
 
     @property
     def parent(self):
